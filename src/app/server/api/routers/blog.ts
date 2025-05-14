88e46233603acb93f13d381192574e33390d8baf
markgyoni <markgyoni@gmail.com>
2025-05-14 09:29:49 -0400

    Actually add TRPC

--- src/app/server/api/routers/blog.ts
+++ src/app/server/api/routers/blog.ts
@@ -1,8 +1,24 @@
 import { createTRPCRouter, publicProcedure } from "@/app/server/api/trpc";
-// import { z } from "zod";
+import { z } from "zod";
 
 export const blogRouter = createTRPCRouter({
-  list: publicProcedure.query(async () => {
-    return true;
-  }),
+  list: publicProcedure
+    .output(
+      z.array(
+        z.object({
+          id: z.number(),
+          title: z.string(),
+          content: z.string(),
+        }),
+      ),
+    )
+    .query(async () => {
+      return [
+        {
+          id: 1,
+          title: "Blog Post 1",
+          content: "This is the content of blog post 1",
+        },
+      ];
+    }),
 });
